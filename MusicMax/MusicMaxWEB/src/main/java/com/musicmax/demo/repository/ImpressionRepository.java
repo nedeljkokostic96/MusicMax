93fed381fdc22355614395e8d4ff44f491f9e8b2
Marko95 <markoadamovic1995@yahoo.com>
2021-05-11 13:54:34 +0200

    clientRepo and impression repo

---
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/repository/ImpressionRepository.java
@@ -0,0 +1,9 @@
+package com.musicmax.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Impression;
+
+public interface ImpressionRepository extends JpaRepository<Impression, Integer> {
+
+}
