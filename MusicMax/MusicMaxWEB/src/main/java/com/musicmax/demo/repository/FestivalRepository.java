f7009926b24eae834e3efa5475b258b9cd9bb3dd
nedeljkokostic96 <nedeljkokostic96@gmail.com>
2021-05-25 17:28:52 +0200

    Added festival repository

---
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/repository/FestivalRepository.java
@@ -0,0 +1,9 @@
+package com.musicmax.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Festival;
+
+public interface FestivalRepository extends JpaRepository<Festival, Integer> {
+
+}
