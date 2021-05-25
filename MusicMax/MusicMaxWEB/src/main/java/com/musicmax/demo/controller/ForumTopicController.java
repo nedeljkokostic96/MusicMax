4edef8c5a7431a271ab17a422f904222836a05f2
nedeljkokostic96 <nedeljkokostic96@gmail.com>
2021-05-25 20:56:30 +0200

    Bug with forum topic controller fixed

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/ForumTopicController.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/ForumTopicController.java
@@ -3,17 +3,20 @@ package com.musicmax.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.service.ForumTopicService;
 
 @RestController
-public class Controller {
+public class ForumTopicController {
 
 	@Autowired
 	private ForumTopicService forumTopicService;
-
+	
+	@PostMapping(value = "/topics", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> saveForumTopic(String json, HttpServletRequest request) {
 		return forumTopicService.saveForumTopic(json, request);
 	}
