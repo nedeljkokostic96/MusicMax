640742f0bddf9d0ea9557a78c43aac482e11432a
Marko95 <markoadamovic1995@yahoo.com>
2021-05-25 21:04:45 +0200

    @T3998:R Added controller method for getting all festivals

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/FestivalController.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/FestivalController.java
@@ -1,16 +1,21 @@
 package com.musicmax.demo.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.service.FestivalService;
+
+import model.Festival;
 
 @RestController
 public class FestivalController {
@@ -21,6 +26,11 @@ public class FestivalController {
 	@PostMapping(value = "/festivals", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> saveNewFestival(@RequestBody String json, HttpServletRequest request) {
 		return festivalService.saveFestival(json, request);
+	}
+	
+	@GetMapping(value = "/festivals")
+	public List<Festival> getAllFestivals(){
+		return festivalService.getAllFestivals();
 	}
 	
 }
