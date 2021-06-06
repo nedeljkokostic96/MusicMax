197d494871103232f57eced9f97eccf4c3d9977c
zzzarko <dmi.zarko.nedeljkovic@student.pmf.uns.ac.rs>
2021-06-06 16:52:38 +0200

    @T4055:R changed route for forum topic controller

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/ForumTopicController.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/ForumTopicController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.service.ForumTopicService;
 
 @RestController
+@RequestMapping(value = "/forum-topic")
 public class ForumTopicController {
 
 	@Autowired
