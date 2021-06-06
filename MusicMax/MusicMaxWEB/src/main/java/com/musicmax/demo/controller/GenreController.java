a44fbdc3185b99823bdd7e10250b605ba2bb91ac
zzzarko <dmi.zarko.nedeljkovic@student.pmf.uns.ac.rs>
2021-06-06 20:44:50 +0200

    @T4058:R changed route for genre request.

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/GenreController.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/GenreController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.repository.GenreRepository;
@@ -12,14 +13,15 @@ import com.musicmax.demo.repository.GenreRepository;
 import model.Genre;
 
 @RestController
+@RequestMapping(value = "/genres")
 public class GenreController {
 
 	@Autowired
 	private GenreRepository genreRepository;
-	
-	@GetMapping(value = "/genres")
+
+	@GetMapping()
 	public List<Genre> getAllGenres() {
 		return genreRepository.findAll();
 	}
-	
+
 }
