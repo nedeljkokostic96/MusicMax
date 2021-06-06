4868d3d6f9cc1e1b407fe527168f24c9f1625293
zzzarko <dmi.zarko.nedeljkovic@student.pmf.uns.ac.rs>
2021-06-06 16:35:41 +0200

    @T4052:R changed route for comment controller

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/CommentControrller.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/CommentControrller.java
@@ -8,17 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.service.CommentService;
 
 @RestController
+@RequestMapping(value = "/comments")
 public class CommentControrller {
 	
 	@Autowired
 	private CommentService commentService;
 	
-	@PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> saveNewComment(@RequestBody String json, HttpServletRequest request) {
 		return commentService.saveComment(json, request);
 	}
