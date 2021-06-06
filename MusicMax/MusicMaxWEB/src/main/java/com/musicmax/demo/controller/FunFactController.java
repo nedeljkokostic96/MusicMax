2f83af7cd1a1682881634e3894481ecded853076
zzzarko <dmi.zarko.nedeljkovic@student.pmf.uns.ac.rs>
2021-06-06 20:40:43 +0200

    @T4057:R

--- MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/FunFactController.java
+++ MusicMax/MusicMaxWEB/src/main/java/com/musicmax/demo/controller/FunFactController.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicmax.demo.service.FunFactService;
 
 @RestController
+@RequestMapping(value = "fun-facts")
 public class FunFactController {
-	
+
 	@Autowired
 	private FunFactService funFactService;
-	
-	@PostMapping(value = "/funFacts", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> saveFunFact(@RequestBody String json) {
 		return funFactService.saveFunFact(json);
 	}
